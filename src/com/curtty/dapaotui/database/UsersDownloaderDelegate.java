109588a03f997160d5bf694023b24c457bfb628a
sleepcry <sleepcry322#gmail.com>
2013-01-05 17:47:30 +0800

    change some name

--- src/com/curtty/dapaotui/database/UsersDownloaderDelegate.java
+++
@@ -1,9 +0,0 @@
-package com.curtty.dapaotui.database;
-
-public interface UsersDownloaderDelegate {
-	
-	public abstract void onUsersDownloadStart(UsersDownloader downloader);
-	public abstract void onUsersDownloadProgress(UsersDownloader downloader, Integer... progress);
-	public abstract void onUsersDownloadSuccess(UsersDownloader downloader, String announcementText, String announcementUrl);
-	public abstract void onUsersDownloadFailure(UsersDownloader downloader, Exception exception);
-}
